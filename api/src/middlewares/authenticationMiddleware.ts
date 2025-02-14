3ca419a021851d4c9705abce83cb23ec36efc830
Victor Motta <victormottas.dev@gmail.com>
2025-02-14 02:21:07 -0300

    feature: added router student to app and fixed errorHandlingMiddleware, authenticationMiddleware and added indext export to protocols

--- api/src/middlewares/authenticationMiddleware.ts
+++ api/src/middlewares/authenticationMiddleware.ts
@@ -2,14 +2,19 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import * as jwt from "jsonwebtoken";
 
-import { unauthorizedError } from "../errors";
+import { unauthorizedError } from "errors";
+import { caseInsensitiveObject } from "utils";
 
 export async function authenticateToken(
     req: AuthenticatedRequest,
     res: Response,
     next: NextFunction
 ) {
-    const authHeader = req.header("Authorization");
+    const headersNormalized = caseInsensitiveObject(req.headers, (key) => key.toLowerCase());
+
+    const authHeaderRaw = headersNormalized["authorization"];
+    const authHeader = Array.isArray(authHeaderRaw) ? authHeaderRaw[0] : authHeaderRaw;
+
     if (!authHeader) return generateUnauthorizedResponse(res);
 
     const token = authHeader.split(" ")[1];
